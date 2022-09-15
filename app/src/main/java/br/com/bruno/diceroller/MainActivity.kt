218dc3a53256885ec0715bf21ad1f70c33154c38
BrunoRijo <bruno_rijo@hotmail.com>
2022-09-14 21:36:55 -0300

    Refactoring and using ViewBinding

--- app/src/main/java/br/com/bruno/diceroller/MainActivity.kt
+++ app/src/main/java/br/com/bruno/diceroller/MainActivity.kt
@@ -16,38 +16,34 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val progressBar : CircularProgressBar = findViewById(R.id.circularProgressBar)
-        val button : Button = findViewById(R.id.btnRollDice)
-        button.setOnClickListener {
-            progressBar.progress = 0f
-            animationBarProgress(progressBar)
+        binding.btnRollDice.setOnClickListener {
+            binding.circularProgressBar.progress = 0f
+            animationBarProgress()
         }
-
     }
 
-    private fun animationBarProgress(progressBar : CircularProgressBar){
-        progressBar.apply {
+    private fun animationBarProgress(){
+        binding.circularProgressBar.apply {
             setProgressWithAnimation(10f, 250)
         }
         rollDice()
     }
 
     private fun rollDice() {
-        val diceNumber : ImageView = findViewById(R.id.image_dice)
-        val drawableResource = when(Random.nextInt(6)+1){
+        binding.imageDice.setImageResource(when(Random.nextInt(6)+1){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
             4 -> R.drawable.dice_4
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
-        }
-        diceNumber.setImageResource(drawableResource)
+        })
     }
 }
